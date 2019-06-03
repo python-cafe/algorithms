67d050ee8d767dd1c622e16e89cebcae4e565532
Hallison Paz <hallpaz@pythoncafe.com.br>
2019-06-02 22:01:27 -0300

    after recording selection sort

---
+++ sorting/sorting.py
@@ -0,0 +1,13 @@
+
+
+def selection_sort(lista):
+    n = len(lista)
+    for j in range(n-1):
+        min_index = j
+        for i in range(j, n):
+            if lista[i] < lista[min_index]:
+                min_index = i
+        if lista[j] > lista[min_index]:
+            aux = lista[j]
+            lista[j] = lista[min_index]
+            lista[min_index] = aux
---
+++ sorting/test.py
@@ -0,0 +1,20 @@
+import random
+from sorting import selection_sort
+
+
+any_numbers = random.sample(range(1, 1000), 42)
+
+already_sorted = [1, 2, 3, 4, 5, 6, 9, 20, 22, 23, 28, 
+                    32, 34, 76, 39, 40, 42, 87, 99, 112]
+
+inversed = [117, 90, 88, 83, 81, 77, 74, 69, 64, 63, 51,
+            50, 49, 42, 41, 34, 32, 29, 28, 22, 16, 8, 6, 5, 3, 1]
+
+repeated = [7, 7, 7, 7, 7, 1, 1, 9, 9, 0, 4, 4, 4, 5, 4, 5, 7, 1,]
+
+if __name__ == "__main__":
+    lista = repeated
+    print(lista)
+    selection_sort(lista)
+    print("\n Ordenado:")
+    print(lista)
