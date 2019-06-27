58fcdcda997a85185665325d04e5d31d530ead71
Hallison Paz <hallpaz@pythoncafe.com.br>
2019-06-27 11:24:31 -0300

    after recording insertion sort

--- sorting/sorting.py
+++ sorting/sorting.py
@@ -1,5 +1,15 @@
 
+def insertion_sort(lista):
+    n = len(lista)
+    for i in range(1, n):
+        chave = lista[i]
+        j = i - 1
+        while j >= 0 and lista[j] > chave:
+            lista[j+1] = lista[j]
+            j = j - 1
+        lista[j+1] = chave
 
+# Vídeo "Bubble Sort": https://youtu.be/GiNPe_678Ms
 def bubble_sort(lista):
     n = len(lista)
     for j in range(n-1):
