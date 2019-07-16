9a0868bfff39dc4565660dd3f1511f87a65eee70
Hallison Paz <hallpaz@pythoncafe.com.br>
2019-07-16 11:14:25 -0300

    after recording merge sort

--- sorting/sorting.py
+++ sorting/sorting.py
@@ -1,3 +1,32 @@
+
+# Merge Sort:
+def mergesort(lista, inicio=0, fim=None):
+    if fim is None:
+        fim = len(lista)
+    if(fim - inicio > 1):
+        meio = (fim + inicio)//2
+        mergesort(lista, inicio, meio)
+        mergesort(lista, meio, fim)
+        merge(lista, inicio, meio, fim)
+
+def merge(lista, inicio, meio, fim):
+    left = lista[inicio:meio]
+    right = lista[meio:fim]
+    top_left, top_right = 0, 0
+    for k in range(inicio, fim):
+        if top_left >= len(left):
+            lista[k] = right[top_right]
+            top_right = top_right + 1
+        elif top_right >= len(right):
+            lista[k] = left[top_left]
+            top_left = top_left + 1
+        elif left[top_left] < right[top_right]:
+            lista[k] = left[top_left]
+            top_left = top_left + 1
+        else:
+            lista[k] = right[top_right]
+            top_right = top_right + 1
+
 # Vídeo "Insertion Sort": https://youtu.be/S5no2qT8_xg
 def insertion_sort(lista):
     n = len(lista)
--- sorting/test.py
+++ sorting/test.py
@@ -1,11 +1,11 @@
 import random
-from sorting import selection_sort, bubble_sort, insertion_sort
+from sorting import selection_sort, bubble_sort, insertion_sort, mergesort
 
 
 any_numbers = random.sample(range(1, 1000), 42)
 
 already_sorted = [1, 2, 3, 4, 5, 6, 9, 20, 22, 23, 28, 
-                    32, 34, 76, 39, 40, 42, 87, 99, 112]
+                    32, 34, 39, 40, 42, 76, 87, 99, 112]
 
 inversed = [117, 90, 88, 83, 81, 77, 74, 69, 64, 63, 51,
             50, 49, 42, 41, 34, 32, 29, 28, 22, 16, 8, 6, 5, 3, 1]
@@ -15,6 +15,6 @@ repeated = [7, 7, 7, 7, 7, 1, 1, 9, 9, 0, 4, 4, 4, 5, 4, 5, 7, 1,]
 if __name__ == "__main__":
     lista = repeated
     print(lista)
-    insertion_sort(lista)
+    mergesort(lista)
     print("\n Ordenado:")
     print(lista)
