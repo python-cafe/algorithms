4742bb26f858d39e2eb078fc0d93db55b2173121
Hallison Paz <hallison.dapaz@gmail.com>
2019-11-16 01:34:26 -0300

    after quicksort recording

--- sorting/sorting.py
+++ sorting/sorting.py
@@ -1,5 +1,29 @@
 
-# Merge Sort:
+# Vídeo "Quick Sort": https://youtu.be/wx5juM9bbFo
+def quicksort(lista, inicio=0, fim=None):
+    if fim is None:
+        fim = len(lista)-1
+    if inicio < fim:
+        p = partition(lista, inicio, fim)
+        # recursivamente na sublista à esquerda (menores)
+        quicksort(lista, inicio, p-1)
+        # recursivamente na sublista à direita (maiores)
+        quicksort(lista, p+1, fim)
+
+def partition(lista, inicio, fim):
+    pivot = lista[fim]
+    i = inicio
+    for j in range(inicio, fim):
+        # j sempre avança, pois representa o elementa em análise
+        # e delimita os elementos maiores que o pivô
+        if lista[j] <= pivot:
+            lista[j], lista[i] = lista[i], lista[j]
+            # incrementa-se o limite dos elementos menores que o pivô
+            i = i + 1
+    lista[i], lista[fim] = lista[fim], lista[i]
+    return i
+
+# Vídeo "Merge Sort": https://youtu.be/S5no2qT8_xg
 def mergesort(lista, inicio=0, fim=None):
     if fim is None:
         fim = len(lista)
--- sorting/test.py
+++ sorting/test.py
@@ -1,5 +1,6 @@
 import random
-from sorting import selection_sort, bubble_sort, insertion_sort, mergesort
+from sorting import selection_sort, bubble_sort, insertion_sort
+from sorting import mergesort, quicksort
 
 
 any_numbers = random.sample(range(1, 1000), 42)
@@ -13,8 +14,16 @@ inversed = [117, 90, 88, 83, 81, 77, 74, 69, 64, 63, 51,
 repeated = [7, 7, 7, 7, 7, 1, 1, 9, 9, 0, 4, 4, 4, 5, 4, 5, 7, 1,]
 
 if __name__ == "__main__":
-    lista = repeated
-    print(lista)
-    mergesort(lista)
-    print("\n Ordenado:")
-    print(lista)
+    test_cases = {'Números aleatórios': any_numbers, 
+                    'Já ordenados': already_sorted, 
+                    'Ordem inversa': inversed, 
+                    'Elementos repetidos': repeated
+                }
+    print("*******************************")
+    for name, lista in test_cases.items():
+        print("\nCaso de teste: {}".format(name))
+        print(lista)
+        quicksort(lista)
+        print("\n Ordenado:")
+        print(lista)
+    print("*******************************")
